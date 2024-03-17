8813d79206d2eb9e8113622e524ee0f3120706e7
nkd10121 <nakada10121@icloud.com>
2024-03-17 15:50:28 +0900

    up

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+
+int main(void) {
+    // 自分の得意な言語で
+    // Let's チャレンジ！！
+    string str = "hello";
+    auto num = str.length();
+    auto temp = str.at(num - 1);
+
+    return 0;
+}

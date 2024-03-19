8c284186857fc7d6ec50375823d3d18a0348357e
nkd10121 <nakada10121@icloud.com>
2024-03-19 16:47:49 +0900

    up

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,12 +1,34 @@
 ﻿#include <iostream>
 using namespace std;
-
 int main(void) {
-    // 自分の得意な言語で
-    // Let's チャレンジ！！
-    string str = "hello";
-    auto num = str.length();
-    auto temp = str.at(num - 1);
+    int days;
+    cin >> days;
+    //cout << days << endl;
+    int min = 0;
+    int max = 0;
 
+    for (int i = 0; i < days; i++)
+    {
+        int h1, h2, h3;
+        cin >> h1 >> h2 >> h3;
+        int add = h1 + h2 + (24 - h3);
+
+        if (min == 0 && max == 0)
+        {
+            min = add;
+            max = add;
+        }
+        else if (add < min)
+        {
+            min = add;
+        }
+        else if (add > max)
+        {
+            max = add;
+        }
+    }
+
+    cout << min << endl;
+    cout << max << endl;
     return 0;
 }
